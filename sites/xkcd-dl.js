5f368ff6feb659022b8e416350546a4a7ed519b7
Prasantacharya <acharp@rpi.edu>
2021-04-05 00:47:26 -0400

    added xkcd for testing the general stuff

--- sites/xkcd-dl.js
+++ sites/xkcd-dl.js
@@ -1,1 +1,22 @@
-// TODO: use api probably
+// TODO: use api for this
+// includes
+common = require("../shared.js")
+
+function downloader(jsonData){
+  let title = jsonData["safe_title"];
+  let img = jsonData["img"];
+  commond.download(img, title);
+}
+
+async function xkcd(num){
+  let url = "https://xkcd.com/" + num + "/info.0.json";
+  common.getFromAPI(url, downloader);
+}
+
+/*
+-- NOTE: needs to be tested
+--       gets the current xkcd webcommic
+*/
+async function xkcd_curr(){
+  common.getFromAPI("http://xkcd.com/info.0.json", downloader);
+}
