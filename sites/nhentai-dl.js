e5e3438310c4604bab5f6ccc600e2a4130c1693c
Prasantacharya <acharp@rpi.edu>
2021-04-11 23:57:43 -0400

    added regex to grab what the image number was

--- sites/nhentai-dl.js
+++ sites/nhentai-dl.js
@@ -5,13 +5,20 @@ const common = require("../shared/shared.js")
 
 */
 
-// document.getElementsByClassName("thumbs")[0].querySelector("img").src;
+// gets the actual url
+/*
+var temp = document.getElementsByClassName("thumb-container");
 
+temp[temp.length - 1].querySelector("img").dataset.src;
+*/
 function nhentai(url){
   await common.scrapeTags(url, () = {
     // this cursed query is for getting the number of pages that are
-    document.querySelectorAll("#info section div:nth-child(8) span a span")[0].innerText;
-  },(pageNum) => {
-    for(i = 0; i < pageNum; i++) common.download("https://i." + url + "/" + i + ".jpg");
+    var temp = document.getElementsByClassName("thumb-container");
+    temp[temp.length - 1].querySelector("img").dataset.src;
+  },(imgURL) => {
+    let pageNum = imgURL.match(/\/[0-9]+t/);
+    let newURL = imgURL.replace(/t\./, 'i.').replaceAll(/t\./ig, ".");
+    for(let i = 0; i < pageNum; i++) common.download(newURL);
   });
 }
